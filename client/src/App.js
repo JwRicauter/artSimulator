967cb4d3516ce86b5e393579e2a5a0786554f911
wricauter <jwricauter@gmail.com>
2020-12-29 01:39:04 -0400

    making a call to our maincontainer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import MainContainer from './components/MainContainer/index';
 
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <MainContainer />
     )
 }
 
